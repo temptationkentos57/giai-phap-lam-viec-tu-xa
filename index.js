c74eaf7ccfa228cb605f6ecbcb9f62e1ff0926b8
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-29 16:18:14 +0200

    Update server listen message for clarity.

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
+  console.log(`Máy chủ hoạt động tại: http://localhost:${PORT}`);
 });
